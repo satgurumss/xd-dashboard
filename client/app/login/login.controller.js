e635bd7572a5e9f7e49e025f9749bb661d64f09c
Shaharyar Maroof <Shaharyar Maroof>
2016-03-27 22:50:25 +0500

    dummy login flow implemented + angular-cookies dependency added

---
+++ client/app/login/login.controller.js
@@ -0,0 +1,33 @@
+(function() {
+    'use strict';
+
+    angular.module('app')
+        .controller('LoginCtrl', ['$scope', '$rootScope', '$http', 'backendApi', '$location','loggedInUser', LoginCtrl])
+
+    function LoginCtrl($scope, $rootScope, $http, backendApi, $location, loggedInUser) {
+        $scope.userName = "";
+        $scope.errorAlert = false;
+
+        $scope.doLogin = function() {
+            var res = backendApi.doLogin()
+                .then(
+                    function(res) {
+                        console.log(res);
+                        var userData = res.data.documents,
+                            user = _.find(userData, function(user){
+                                            return user.fields.account_s[0] === $scope.userName;
+                                        });
+                        console.log(user);
+                        if (typeof user != 'undefined') {
+                            loggedInUser.setCurrentUser(user);
+                            $rootScope.$emit("reloadHeader",{});
+                            $location.url("/dashboard");
+                        } else {
+                            $scope.errorAlert = true;
+                        }
+                    }
+                )
+        }
+    }
+
+})();
