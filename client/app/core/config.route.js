cc3be0c94c05e7e1fcd50f1cde8c00beee20e5b0
Usman_Saeed <usman.saeed@emumba.com>
2016-03-28 18:11:51 +0500

    dashboard - slim scroll, login service, styling changes

--- client/app/core/config.route.js
+++ client/app/core/config.route.js
@@ -1,52 +1,63 @@
-(function () {
-    'use strict';
+(function() {
+  'use strict';
 
-    angular.module('app')
-        .config(['$routeProvider', '$locationProvider', function($routeProvider, $locationProvider) {
-            var routes, setRoutes;
+  angular.module('app')
+    .config(['$routeProvider', '$locationProvider', function($routeProvider, $locationProvider) {
+      var routes, setRoutes;
 
-            console.log($locationProvider);
-            routes = [
-                'dashboard',
-                'search-result',
-                'ui/typography', 'ui/buttons', 'ui/icons', 'ui/grids', 'ui/widgets', 'ui/components', 'ui/boxes', 'ui/timeline', 'ui/nested-lists', 'ui/pricing-tables', 'ui/maps',
-                'table/static', 'table/dynamic', 'table/responsive',
-                'form/elements', 'form/layouts', 'form/validation', 'form/wizard',
-                'chart/echarts', 'chart/echarts-line', 'chart/echarts-bar', 'chart/echarts-pie', 'chart/echarts-scatter', 'chart/echarts-more',
-                'page/404', 'page/500', 'page/blank', 'page/forgot-password', 'page/invoice', 'page/lock-screen', 'page/profile', 'page/invoice', 'page/signin', 'page/signup', 'page/about', 'page/services', 'page/contact',
-                'mail/compose', 'mail/inbox', 'mail/single',
-                'app/tasks', 'app/calendar'
-            ]
+      console.log($locationProvider);
+      routes = [
+        'dashboard',
+        'search-result',
+        'ui/typography', 'ui/buttons', 'ui/icons', 'ui/grids', 'ui/widgets', 'ui/components', 'ui/boxes', 'ui/timeline', 'ui/nested-lists', 'ui/pricing-tables', 'ui/maps',
+        'table/static', 'table/dynamic', 'table/responsive',
+        'form/elements', 'form/layouts', 'form/validation', 'form/wizard',
+        'chart/echarts', 'chart/echarts-line', 'chart/echarts-bar', 'chart/echarts-pie', 'chart/echarts-scatter', 'chart/echarts-more',
+        'page/404', 'page/500', 'page/blank', 'page/forgot-password', 'page/invoice', 'page/lock-screen', 'page/profile', 'page/invoice', 'page/signin', 'page/signup', 'page/about', 'page/services', 'page/contact',
+        'mail/compose', 'mail/inbox', 'mail/single',
+        'app/tasks', 'app/calendar'
+      ]
 
-            // $locationProvider.html5Mode(true);
-            // enable html5Mode for pushstate ('#'-less URLs)
-            // $locationProvider.html5Mode(true);
-            // $locationProvider.hashPrefix('!');
+      // $locationProvider.html5Mode(true);
+      // enable html5Mode for pushstate ('#'-less URLs)
+      // $locationProvider.html5Mode(true)
+      // $locationProvider.hashPrefix('!');
 
-            setRoutes = function(route) {
-                var config, url;
-                url = '/' + route;
-                config = {
-                    templateUrl: 'app/' + route + '.html'
-                };
-                $routeProvider.when(url, config);
-                return $routeProvider;
-            };
+      setRoutes = function(route) {
+        var config, url;
+        url = '/' + route;
+        config = {
+          templateUrl: 'app/' + route + '.html'
+        };
+        $routeProvider.when(url, config);
+        return $routeProvider;
+      };
 
-            routes.forEach(function(route) {
-                return setRoutes(route);
-            });
+      routes.forEach(function(route) {
+        return setRoutes(route);
+      });
 
-            $routeProvider
-                // .when('/', {redirectTo: 'page/signin'})
-                .when('/', {redirectTo: 'signin'})
-                .when('/signin', {templateUrl: 'app/page/signin.html'})
-                .when('/dashboard', {templateUrl: 'app/dashboard/dashboard.html'})
-                .when('/search-result', {templateUrl: 'app/search/search-result.html'})
-                .when('/404', {templateUrl: 'app/page/404.html'})
-                .otherwise({ redirectTo: '/404'});
+      $routeProvider
+      // .when('/', {redirectTo: 'page/signin'})
+        .when('/', {
+          redirectTo: 'signin'
+        })
+        .when('/signin', {
+          templateUrl: 'app/page/signin.html'
+        })
+        .when('/dashboard', {
+          templateUrl: 'app/dashboard/dashboard.html'
+        })
+        .when('/search-result', {
+          templateUrl: 'app/search/search-result.html'
+        })
+        .when('/404', {
+          templateUrl: 'app/page/404.html'
+        })
+        .otherwise({
+          redirectTo: '/404'
+        });
 
-        }]
-    );
+    }]);
 
 })();
