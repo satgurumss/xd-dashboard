71587e23aaf0889f34c5e51c9e2372fcd19c6cb3
Shaharyar Maroof <Shaharyar Maroof>
2016-04-02 15:31:36 +0500

    dashboard uris added + tables formatted + search page thumbnails check added

--- client/app/dashboard/dashboard.controller.js
+++ client/app/dashboard/dashboard.controller.js
@@ -1254,12 +1254,50 @@ var clickEvent;
         return res.data;
       });
     }
+
+    $scope.abbreviateNumber = function(num, digits) {
+      var si = [{
+          value: 1E18,
+          symbol: "E"
+        }, {
+          value: 1E15,
+          symbol: "P"
+        }, {
+          value: 1E12,
+          symbol: "T"
+        }, {
+          value: 1E9,
+          symbol: "B"
+        }, {
+          value: 1E6,
+          symbol: "M"
+        }, {
+          value: 1E3,
+          symbol: "K"
+        }],
+        i;
+
+      num = typeof num !== "undefined" ? parseInt(num) : "";
+      if( num > 0 && num != "" ){
+
+        for (i = 0; i < si.length; i++) {
+          if (num >= si[i].value) {
+            return (num / si[i].value).toFixed(digits).replace(/\.0+$|(\.[0-9]*[1-9])0+$/, "$1") + si[i].symbol;
+          }
+        }
+        return num.toString();
+      }
+      else{
+        return num;
+      }
+    }
   }
 
 })();
 
 angular.module('app')
   .animation('.slide', [
+
     function() {
       var support;
       support = jQuery.keyframe.isSupported();
