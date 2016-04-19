79072bb0f669d03011e9d686e6dd08e07be5c610
Shaharyar Maroof <Shaharyar Maroof>
2016-04-19 17:07:18 +0500

    chart layout updated

--- client/app/dashboard/dashboard.controller.js
+++ client/app/dashboard/dashboard.controller.js
@@ -14,122 +14,12 @@ var clickEvent;
 
   function DashboardCtrl($scope, $http, $location, backendApi, loggedInUser, $timeout) {
 
-    /*$scope.lineOptions = {
-      options:{chart: {
-        type: 'spline'
-      }},
-      title: {
-        text: 'Snow depth at Vikjafjellet, Norway'
-      },
-      subtitle: {
-        text: 'Irregular time data in Highcharts JS'
-      },
-      xAxis: {
-        type: 'datetime',
-        dateTimeLabelFormats: { // don't display the dummy year
-          month: '%e. %b',
-          year: '%b'
-        },
-        title: {
-          text: 'Date'
-        }
-      },
-      yAxis: {
-        title: {
-          text: 'Snow depth (m)'
-        },
-        min: 0
-      },
-      tooltip: {
-        headerFormat: '<b>{series.name}</b><br>',
-        pointFormat: '{point.x:%e. %b}: {point.y:.2f} m'
-      },
-
-      plotOptions: {
-        spline: {
-          marker: {
-            enabled: true
-          }
-        }
-      },
-
-      series: [{
-        name: 'Winter 2012-2013',
-        // Define the data points. All series have a dummy year
-        // of 1970/71 in order to be compared on the same x axis. Note
-        // that in JavaScript, months start at 0 for January, 1 for February etc.
-        data: [
-          [Date.UTC(1970, 9, 21), 0],
-          [Date.UTC(1970, 10, 4), 0.28],
-          [Date.UTC(1970, 10, 9), 0.25],
-          [Date.UTC(1970, 10, 27), 0.2],
-          [Date.UTC(1970, 11, 2), 0.28],
-          [Date.UTC(1970, 11, 26), 0.28],
-          [Date.UTC(1970, 11, 29), 0.47],
-          [Date.UTC(1971, 0, 11), 0.79],
-          [Date.UTC(1971, 0, 26), 0.72],
-          [Date.UTC(1971, 1, 3), 1.02],
-          [Date.UTC(1971, 1, 11), 1.12],
-          [Date.UTC(1971, 1, 25), 1.2],
-          [Date.UTC(1971, 2, 11), 1.18],
-          [Date.UTC(1971, 3, 11), 1.19],
-          [Date.UTC(1971, 4, 1), 1.85],
-          [Date.UTC(1971, 4, 5), 2.22],
-          [Date.UTC(1971, 4, 19), 1.15],
-          [Date.UTC(1971, 5, 3), 0]
-        ]
-      }, {
-        name: 'Winter 2013-2014',
-        data: [
-          [Date.UTC(1970, 9, 29), 0],
-          [Date.UTC(1970, 10, 9), 0.4],
-          [Date.UTC(1970, 11, 1), 0.25],
-          [Date.UTC(1971, 0, 1), 1.66],
-          [Date.UTC(1971, 0, 10), 1.8],
-          [Date.UTC(1971, 1, 19), 1.76],
-          [Date.UTC(1971, 2, 25), 2.62],
-          [Date.UTC(1971, 3, 19), 2.41],
-          [Date.UTC(1971, 3, 30), 2.05],
-          [Date.UTC(1971, 4, 14), 1.7],
-          [Date.UTC(1971, 4, 24), 1.1],
-          [Date.UTC(1971, 5, 10), 0]
-        ]
-      }, {
-        name: 'Winter 2014-2015',
-        data: [
-          [Date.UTC(1970, 10, 25), 0],
-          [Date.UTC(1970, 11, 6), 0.25],
-          [Date.UTC(1970, 11, 20), 1.41],
-          [Date.UTC(1970, 11, 25), 1.64],
-          [Date.UTC(1971, 0, 4), 1.6],
-          [Date.UTC(1971, 0, 17), 2.55],
-          [Date.UTC(1971, 0, 24), 2.62],
-          [Date.UTC(1971, 1, 4), 2.5],
-          [Date.UTC(1971, 1, 14), 2.42],
-          [Date.UTC(1971, 2, 6), 2.74],
-          [Date.UTC(1971, 2, 14), 2.62],
-          [Date.UTC(1971, 2, 24), 2.6],
-          [Date.UTC(1971, 3, 2), 2.81],
-          [Date.UTC(1971, 3, 12), 2.63],
-          [Date.UTC(1971, 3, 28), 2.77],
-          [Date.UTC(1971, 4, 5), 2.68],
-          [Date.UTC(1971, 4, 10), 2.56],
-          [Date.UTC(1971, 4, 15), 2.39],
-          [Date.UTC(1971, 4, 20), 2.3],
-          [Date.UTC(1971, 5, 5), 2],
-          [Date.UTC(1971, 5, 10), 1.85],
-          [Date.UTC(1971, 5, 15), 1.49],
-          [Date.UTC(1971, 5, 23), 1.08]
-        ]
-      }]
-    };*/
-
     //This is not a highcharts object. It just looks a little like one!
     $scope.chartConfig = {
       options: {
-        colors: ["#28bdc6", "#90e4ad", "#cce679", "#7798BF", "#aaeeee", "#ff0066", "#eeaaee",
-          "#55BF3B", "#DF5353", "#7798BF", "#aaeeee"],
+        colors: ["#28bdc6", "rgba(144,228,173, .5)", "rgba(204, 230, 121, .5)"],
         chart: {
+          height: 260,
           backgroundColor: {
             linearGradient: {
               x1: 1,
@@ -178,7 +68,8 @@ var clickEvent;
 
         legend: {
           itemStyle: {
-            color: '#E0E0E3'
+            color: '#E0E0E3',
+            fontSize: '10px'
           },
           itemHoverStyle: {
             color: '#FFF'
@@ -186,11 +77,8 @@ var clickEvent;
           itemHiddenStyle: {
             color: '#606063'
           },
-          layout: 'vertical',
-          align: 'left',
-          verticalAlign: 'top',
-          padding: 50,
-          floating: true,
+          layout: 'horizontal',
+          align:"center",
           borderWidth: 0
         },
 
@@ -298,12 +186,13 @@ var clickEvent;
       //The below properties are watched separately for changes.
       //Title configuration (optional)
       title: {
-        text: 'Revenue, 2015',
+        text: '<center><span class="h4">$100,000</span><br><span class="h6">Current Revenue</span></center>',
         style: {
           color: '#E0E0E3',
           textTransform: 'uppercase',
           fontSize: '20px'
-        }
+        },
+        useHTML:true
       },
 
       subtitle: {
@@ -322,6 +211,7 @@ var clickEvent;
         },
         lineColor: '#707073',
         minorGridLineColor: '#505053',
+        minorTickInterval: 'auto',
         tickColor: '#707073',
         tickWidth: 1,
         title: {
@@ -363,11 +253,11 @@ var clickEvent;
         }, {
             name: 'Target',
             data: [1.2, 17.0, 19, 8.1],
-            marker: {symbol: 'circle', fillColor: '#303031', lineWidth:1, lineColor: '#90e4ad'}
+            marker: {symbol: 'circle', fillColor: '#303031', lineWidth:1, lineColor: 'rgba(144,228,173, .6)'}
         }, {
             name: 'Last Year',
             data: [3.2, 13.5, 14.3, 12.2],
-            marker: {symbol: 'circle', fillColor: '#303031', lineWidth:1, lineColor: '#cce679'}
+            marker: {symbol: 'circle', fillColor: '#303031', lineWidth:1, lineColor: 'rgba(204, 230, 121, .6)'}
         }]
     };
 
