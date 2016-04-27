b9df837bd65e086f11dc50555830e31a33daca15
Shaharyar Maroof <Shaharyar Maroof>
2016-04-27 14:17:03 +0500

    made slide dynamic + line chart series label updated

--- client/app/dashboard/financial/financial.dashboard.controller.js
+++ client/app/dashboard/financial/financial.dashboard.controller.js
@@ -27,8 +27,8 @@
       },
       scale: [3, 6, 9, 12]
     };
-    
-    $scope.value = "3";
+
+    $scope.monthsToDisplay = "3";
 
     $scope.chartConfig = {
       colors: ["#28bdc6", "rgba(144,228,173, .3)", "rgba(204, 230, 121, .3)"],
@@ -152,8 +152,7 @@
       },
 
       xAxis: {
-        categories: ['JUL', 'OCT', 'JAN', 'APR'],
-        tickWidth: 0,
+        categories: ['FEB', 'MAR', 'APR'],
         title: {
           style: {
             color: '#A0A0A3'
@@ -170,10 +169,9 @@
         tickColor: '#707073',
         tickInterval: 1,
         tickmarkPlacement: "on",
-        min: 0.5,
-        max: 2.5,
-        startOnTick: false,
-        endOnTick: false,
+        min: 0,
+        startOnTick: true,
+        endOnTick: true,
         minPadding: 0,
         maxPadding: 0,
         align: "left"
@@ -183,7 +181,7 @@
         type: 'area',
         fillColor: "rgba(40, 189, 198, 0.3)",
         name: 'Revenue',
-        data: [5.0, 14.5, 18.2, 15.2],
+        data: [5.0, 14.5, 18.2],
         marker: {
           symbol: 'circle',
           fillColor: '#303031',
@@ -193,8 +191,8 @@
       }, {
         type: 'area',
         fillColor: "rgba(144,228,173, 0.3)",
-        name: 'Target',
-        data: [1.2, 17.0, 19, 8.1],
+        name: 'Forecast',
+        data: [1.2, 17.0, 19],
         marker: {
           symbol: 'circle',
           fillColor: '#303031',
@@ -205,7 +203,7 @@
         type: 'area',
         fillColor: "rgba(204, 230, 121, 0.3)",
         name: 'Last Year',
-        data: [3.2, 13.5, 14.3, 12.2],
+        data: [3.2, 13.5, 14.3],
         marker: {
           symbol: 'circle',
           fillColor: '#303031',
@@ -214,6 +212,83 @@
         }
       }]
     };
+
+    $scope.updateChart = function() {
+      var categories = [],
+        generateRandomSeries = function(max) {
+          var rnd = []
+          for (var i = 0; i < max; i++) {
+            rnd.push(Math.floor(Math.random() * 20) + 1)
+          }
+          return rnd;
+        }
+
+      switch ($scope.monthsToDisplay) {
+        case "3":
+          categories = ['FEB', 'MAR', 'APR']
+          $scope.chartConfig.xAxis.categories = categories;
+          $scope.chartConfig.xAxis.max = (categories.length) - 1.5;
+          $scope.chartConfig.series[0].data = [];
+          $scope.chartConfig.series[0].data = [5.0, 14.5, 18.2];
+
+          $scope.chartConfig.series[1].data = [];
+          $scope.chartConfig.series[1].data = [1.2, 17.0, 19];
+
+          $scope.chartConfig.series[2].data = [];
+          $scope.chartConfig.series[2].data = [3.2, 13.5, 14.3];
+
+
+          break
+        case "6":
+          categories = ['NOV', 'DEC', 'JAN', 'FEB', 'MAR', 'APR']
+          $scope.chartConfig.xAxis.categories = categories;
+          $scope.chartConfig.xAxis.max = (categories.length) - 1.5;
+
+          $scope.chartConfig.series[0].data = [];
+          $scope.chartConfig.series[0].data = [6.0, 7.8, 16.0, 6.0, 14.5, 18.2];
+
+          $scope.chartConfig.series[1].data = [];
+          $scope.chartConfig.series[1].data = [5.0, 10, 14.3, 5.2, 17.0, 19];
+
+          $scope.chartConfig.series[2].data = [];
+          $scope.chartConfig.series[2].data = [8.0, 5.5, 10.7, 7.2, 13.5, 14.3];
+
+
+          break;
+        case "9":
+          categories = ['AUG', 'SEP', 'OCT', 'NOV', 'DEC', 'JAN', 'FEB', 'MAR', 'APR']
+          $scope.chartConfig.xAxis.categories = categories;
+          $scope.chartConfig.xAxis.max = (categories.length) - 1.5;
+
+          $scope.chartConfig.series[0].data = [];
+          $scope.chartConfig.series[0].data = [10.8, 9.5, 11, 6.0, 7.8, 16.0, 5.0, 14.5, 18.2];
+
+          $scope.chartConfig.series[1].data = [];
+          $scope.chartConfig.series[1].data = [11, 7.4, 9, 5.0, 10, 14.3, 1.2, 17.0, 19];
+
+          $scope.chartConfig.series[2].data = [];
+          $scope.chartConfig.series[2].data = [12, 10, 15, 8.0, 5.5, 10.7, 3.2, 13.5, 14.3];
+
+          break;
+        case "12":
+          categories = ['MAY', 'JUN', 'JUL', 'AUG', 'SEP', 'OCT', 'NOV', 'DEC', 'JAN', 'FEB', 'MAR', 'APR']
+          $scope.chartConfig.xAxis.categories = categories;
+          $scope.chartConfig.xAxis.max = (categories.length) - 1.5;
+
+          $scope.chartConfig.series[0].data = [];
+          $scope.chartConfig.series[0].data = [15, 5.0, 14.5, 10.8, 9.5, 11, 6.0, 7.8, 16.0, 5.0, 14.5, 18.2];
+
+          $scope.chartConfig.series[1].data = [];
+          $scope.chartConfig.series[1].data = [10, 6.5, 12, 11, 7.4, 9, 5.0, 10, 14.3, 1.2, 17.0, 19];
+
+          $scope.chartConfig.series[2].data = [];
+          $scope.chartConfig.series[2].data = [12, 7, 11.7, 12, 10, 15, 8.0, 5.5, 10.7, 3.2, 13.5, 14.3];
+
+
+
+          break;
+      }
+    }
 
   }
 
